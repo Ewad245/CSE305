87ab3bb396fc2cbc1425ba5c419ab1452a0ca12a
Hnam181 <nguyenhoainam1812000@gmail.com>
2023-11-11 16:02:46 +0700

    n

--- src/cse305/Tenant.java
+++ src/cse305/Tenant.java
@@ -1,17 +1,40 @@
+package lab_cse305;
 
-package cse305;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tenant extends User {
 
-public class Tenant extends User{
-    boolean isTenant;
-    int NumofContracts;
-    //Constructor
-    //default
-    //Method
-    public void CreateRentalContract() {
-        
+    private List<RentalContract> rentedProperties;
+
+    public Tenant(String userID, String firstName, String lastName, String email, String password) {
+        super(userID, firstName, lastName, email, password);
+        rentedProperties = new ArrayList<>();
     }
-    public void TerminateRentalContract() {
-        
+
+    public Tenant() {
+    }
+    
+    
+    public List<RentalContract> getRentedProperties() {
+        return new ArrayList<>(rentedProperties); // Return a copy to prevent external modification
+    }
+
+    public void createRentalContract(Property property, int durationMonths) {
+       
+        RentalContract newContract = new RentalContract(property, durationMonths);
+        rentedProperties.add(newContract);
+
+        System.out.println("Rental contract created successfully.");
+    }
+
+    public void terminateRentalContract(RentalContract contract) {
+        if (rentedProperties.contains(contract)) {
+            contract.terminateContract();
+            rentedProperties.remove(contract);
+            System.out.println("Rental contract terminated successfully.");
+        } else {
+            System.out.println("Invalid rental contract. Unable to terminate.");
+        }
     }
 }
