87a6d2d131e3acc3fdfa8bf5a47a3d182a8d920a
NgoLuc11 <luc.ngo.set18@eiu.edu.vn>
2023-11-18 13:12:10 +0700

    j

--- src/cse305/PropertyOwner.java
+++ src/cse305/PropertyOwner.java
@@ -1,26 +1,49 @@
-package cse305;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_cse305;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class PropertyOwner extends User{
+/**
+ *
+ * @author Student
+ */
+public class PropertyOwner extends User {
+    private List<Property> ownedProperties;
 
-    private String AuthenticationKey;
-    private int NumofProperties;
-
-    //Contructors
-    //Default
-    //Method
-    public ArrayList ListProperty (ArrayList List) {
-        return List;
-    }
-    public void CreateProperty() {
-        
-    } 
-    public void UpdateProperty() {
-        
-    }
-    public void DeleteProperty() {
-        
+    public PropertyOwner(List<Property> ownedProperties) {
+        this.ownedProperties = ownedProperties;
     }
 
+    public void listProperties() {
+        System.out.println("Owned Properties:");
+        for (Property property : ownedProperties) {
+            System.out.println(property.toString());
+        }
+    }
+
+    public void createProperty(String propertyName, double rentalRate, String location) {
+        int propertyID = generatePropertyID();
+        Property newProperty = new Property(propertyID, propertyName, rentalRate);
+        ownedProperties.add(newProperty);
+        System.out.println("Property created successfully.");
+    }
+
+    public void updateProperty(Property property, String newPropertyID, double newRentalRate, String newLocation) {
+        property.setPropertyID(Integer.parseInt(newPropertyID));
+        property.setRentalRate(newRentalRate);
+        
+        System.out.println("Property updated successfully.");
+    }
+
+    public void deleteProperty(Property property) {
+        ownedProperties.remove(property);
+        System.out.println("Property deleted successfully.");
+    }
+
+    private int generatePropertyID() {
+        return (int) (Math.random() * 1000);
+    }
 }
